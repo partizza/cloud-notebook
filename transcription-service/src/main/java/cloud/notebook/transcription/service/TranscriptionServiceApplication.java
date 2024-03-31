6b302739e8f608d6eccef67f906cf7cd54ad462b
partiza <bufferdp@gmail.com>
2024-03-31 15:42:28 +0300

    transcription-service: inited

---
+++ transcription-service/src/main/java/cloud/notebook/transcription/service/TranscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package cloud.notebook.transcription.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscriptionServiceApplication.class, args);
+	}
+
+}
---
+++ transcription-service/src/test/java/cloud/notebook/transcription/service/TranscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.notebook.transcription.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
