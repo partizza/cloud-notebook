cdf60602a320603b82282471baaa67c722f24fea
partiza <bufferdp@gmail.com>
2024-04-13 13:32:20 +0300

    api-gateway added

---
+++ api-gateway/src/main/java/cloud/notebook/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,27 @@
+package cloud.notebook.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+import static org.springframework.cloud.gateway.server.mvc.predicate.GatewayRequestPredicates.path;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> route() {
+        return GatewayRouterFunctions.route("audio-service")
+                .route(path("/audio/**"), http("http://audio-service:8080"))
+                .build();
+    }
+
+}
---
+++ api-gateway/src/test/java/cloud/notebook/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.notebook.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
